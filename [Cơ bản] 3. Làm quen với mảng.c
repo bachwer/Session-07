e6d069e3f7dd840dd8602f1796529230d53a0ca3
bachwer <110678064+bachwer@users.noreply.github.com>
2024-11-27 15:50:03 +0700

    Create [Cơ bản] 3. Làm quen với mảng.c

---
+++ [Cơ bản] 3. Làm quen với mảng.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num[] = {2,3,1,3,4};
+    
+    for(int i = 0; i < 5; i++){
+       if(num[i] % 2 == 0){
+        printf("%d\n", num[i]);
+       }
+    }
+    
+    return 0;
+}
