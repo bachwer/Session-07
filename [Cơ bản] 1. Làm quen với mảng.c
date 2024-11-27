6e008cfc3fabd0003ff71e5adbf8c4f933998514
bachwer <110678064+bachwer@users.noreply.github.com>
2024-11-27 15:32:05 +0700

    Create [Cơ bản] 1. Làm quen với mảng.c

---
+++ [Cơ bản] 1. Làm quen với mảng.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int month[12];
+    
+    for(int i = 1; i <= 12; i++){
+        month[i] = i;
+        printf("%d\n", month[i]);
+    }
+    
+  
+    return 0;
+}
+
+
+
