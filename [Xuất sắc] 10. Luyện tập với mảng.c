9b6c8e4b2abb2df9bd95f24dd35a956347722e6e
bachwer <110678064+bachwer@users.noreply.github.com>
2024-11-28 12:00:23 +0700

    Update [Xuất sắc] 10. Luyện tập với mảng.c

--- [Xuất sắc] 10. Luyện tập với mảng.c
+++ [Xuất sắc] 10. Luyện tập với mảng.c
@@ -1,45 +1,26 @@
 #include <stdio.h>
-
 int main() {
    int i;
+   int u;
    printf("Enter your number: ");
    scanf("%d", &i);
-   
-   int u;
-   int l = 1;
-   int y = 1;
-   int z = 0;
    int num[i];
- while(y <= i){
-       u = 0;
-     for(int k = 1; k <= l; k++){
-       if(l % k == 0 ){
-           u++;
+   int y = 0;
+   for(int k = 0; k < i; k++){
+       printf("nhập số phần tử %d:  ", k + 1);
+       scanf("%d", &num[k]);
+   }
+   while(y < i){
+    u = 0;
+    for(int k = 1; k <= num[y]; k++){
+       if(num[y] % k == 0){
+        u++;
        }
+       }
+       if(u == 2){
+           printf("%d  ", num[y]);
+       }
+       y++;
    }
-   if(u == 2){
-       num[z] = l;
-        z++;
-        y++;
-   }
- l++;
-}
-
-for(int o = 0; o < i; o++){
-    printf("%d\n", num[o]);
-}
     return 0;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
