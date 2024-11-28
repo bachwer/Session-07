142e430f803a3c739a27bb521af15e669eeb2f45
bachwer <110678064+bachwer@users.noreply.github.com>
2024-11-28 11:25:23 +0700

    Update [Cơ bản] 3. Làm quen với mảng.c

--- [Cơ bản] 3. Làm quen với mảng.c
+++ [Cơ bản] 3. Làm quen với mảng.c
@@ -5,6 +5,8 @@ int main() {
     for(int i = 0; i < 5; i++){
        if(num[i] % 2 == 0){
         printf("%d\n", num[i]);
+       }else{
+           printf("Mang ko chua so chan !!");
        }
     }
     
